d68f4975830607df4cee523055c9de95829edb85
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2026-02-14 23:33:07 +0100

    add mutex

--- include/coopsync_tbb/coopsync_tbb.hpp
+++ include/coopsync_tbb/coopsync_tbb.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "coopsync_tbb/latch.hpp"
+#include "coopsync_tbb/mutex.hpp"
 
 /// @brief Main namespace of the library, containing all public APIs.
 namespace coopsync_tbb {}
---
+++ tests/mutex_test.cpp
@@ -0,0 +1,35 @@
+#include "coopsync_tbb/mutex.hpp"
+
+#include <gtest/gtest.h>
+#include <oneapi/tbb/parallel_for.h>
+
+TEST(Mutex, NoContentionTryLock) {
+    auto m = coopsync_tbb::mutex{};
+    ASSERT_TRUE(m.try_lock());
+    ASSERT_FALSE(m.try_lock());
+    m.unlock();
+    ASSERT_TRUE(m.try_lock());
+    m.unlock();
+}
+
+TEST(Mutex, NoContentionLock) {
+    auto m = coopsync_tbb::mutex{};
+    m.lock();
+    ASSERT_FALSE(m.try_lock());
+    m.unlock();
+    ASSERT_TRUE(m.try_lock());
+    m.unlock();
+}
+
+TEST(Mutex, MutualExclusionParallelIncrement) {
+    auto m = coopsync_tbb::mutex{};
+    auto counter = 0;
+    const auto n = 20000;
+
+    tbb::parallel_for(0, n, [&](int) {
+        coopsync_tbb::scoped_lock lock(m);
+        ++counter;
+    });
+
+    ASSERT_EQ(counter, n);
+}
