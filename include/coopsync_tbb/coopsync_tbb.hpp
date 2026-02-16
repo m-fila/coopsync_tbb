62db50f8e9cab2d414b1f55c7d43a6c440024b89
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2026-02-16 02:45:03 +0100

    add missing header

--- include/coopsync_tbb/coopsync_tbb.hpp
+++ include/coopsync_tbb/coopsync_tbb.hpp
@@ -2,6 +2,7 @@
 
 #include "coopsync_tbb/latch.hpp"
 #include "coopsync_tbb/mutex.hpp"
+#include "coopsync_tbb/scoped_lock.hpp"
 
 /// @brief Main namespace of the library, containing all public APIs.
 namespace coopsync_tbb {}
