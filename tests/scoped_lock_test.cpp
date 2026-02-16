1a59d607a9021b04366f8912823b19e5dc1e7051
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2026-02-16 02:15:57 +0100

    add more tests for scoped_lock

--- tests/scoped_lock_test.cpp
+++ tests/scoped_lock_test.cpp
@@ -1,6 +1,9 @@
 #include "coopsync_tbb/scoped_lock.hpp"
 
 #include <gtest/gtest.h>
+#include <oneapi/tbb/spin_mutex.h>
+
+#include <mutex>
 
 struct MutexMock {
     void lock() { ++lock_count; }
@@ -20,4 +23,20 @@ TEST(ScopedLock, MutexMock) {
     }
     ASSERT_EQ(m.lock_count, 1);
     ASSERT_EQ(m.unlock_count, 1);
+}
+
+TEST(ScopedLock, StdMutex) {
+    auto m = std::mutex{};
+    {
+        auto lock = coopsync_tbb::scoped_lock(m);
+        ASSERT_FALSE(m.try_lock());
+    }
+}
+
+TEST(ScopedLock, TBBSpinMutex) {
+    auto m = tbb::spin_mutex{};
+    {
+        auto lock = coopsync_tbb::scoped_lock(m);
+        ASSERT_FALSE(m.try_lock());
+    }
 }
