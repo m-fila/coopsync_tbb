c7688e16eb8da2a23f243f7e0c7e54431bd03bb8
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2026-02-17 16:55:42 +0100

    add semaphore specialization for 1

--- include/coopsync_tbb/semaphore.hpp
+++ include/coopsync_tbb/semaphore.hpp
@@ -135,6 +135,67 @@ class counting_semaphore {
     detail::wait_queue m_waiters;
 };
 
+/// @brief A specialization of a counting_semaphore handling only a single
+/// permit. Same as @c binary_semaphore.
+///
+template <>
+class counting_semaphore<1> {
+    public:
+    /// @brief Constructs a counting_semaphore with the specified initial count.
+    /// @param desired The initial count. Must be 0 or 1.
+    explicit counting_semaphore(std::ptrdiff_t desired);
+
+    /// @brief counting_semaphore is not copy-constructible.
+    counting_semaphore(const counting_semaphore&) = delete;
+
+    /// @brief counting_semaphore is not copy-assignable.
+    counting_semaphore& operator=(const counting_semaphore&) = delete;
+
+    /// @brief counting_semaphore is not move-constructible.
+    counting_semaphore(counting_semaphore&&) = delete;
+
+    /// @brief counting_semaphore is not move-assignable.
+    counting_semaphore& operator=(counting_semaphore&&) = delete;
+
+    /// @brief Destroys the counting_semaphore.
+    /// @note The destructor must not be called while there are still tasks
+    /// waiting on the counting_semaphore. The destructor does not notify or
+    /// resume any waiting tasks.
+    ~counting_semaphore() = default;
+
+    /// @brief Releases the semaphore incrementing the counter by the specified
+    /// update. If there are tasks suspended on the semaphore and the counter
+    /// becomes greater than zero, one of them is resumed.
+    /// @param update The value to increment the counter by. Must be
+    /// non-negative,  all positive values are treated as 1.
+    void release(std::ptrdiff_t update = 1);
+
+    /// @brief Acquires the semaphore. If the semaphore is already acquired the
+    /// calling task is suspended until the semaphore is released.
+    /// @note The suspended task must remain valid until it is resumed.
+    /// @note The suspended task must be resumed before the binary_semaphore
+    /// is destroyed.
+    void acquire();
+
+    /// @brief Attempts to acquire the semaphore without suspending.
+    /// @return true if the semaphore was successfully acquired, false
+    /// otherwise.
+    bool try_acquire();
+
+    /// @brief Returns the maximum value for the counter of the binary
+    /// semaphore.
+    /// @return Always returns 1.
+    constexpr static std::ptrdiff_t max() noexcept;
+
+    private:
+    using waiter_t = tbb::task::suspend_point;
+
+    std::atomic<bool> m_available;
+    tbb::spin_mutex m_waiters_mutex;
+    detail::intrusive_list<waiter_t> m_waiters;
+};
+
+/// @brief An alias for @c counting_semaphore<1>.
 using binary_semaphore = counting_semaphore<1>;
 
 template <std::ptrdiff_t LeastMaxValue>
@@ -185,6 +246,71 @@ inline void counting_semaphore<LeastMaxValue>::release(std::ptrdiff_t update) {
 
     // Wake up to `update` waiters; any unused permits stay in m_counter.
     m_waiters.resume_n(update);
+}
+
+inline counting_semaphore<1>::counting_semaphore(std::ptrdiff_t desired)
+    : m_available(desired) {
+    assert(desired == 0 || desired == 1);
+}
+
+inline constexpr std::ptrdiff_t counting_semaphore<1>::max() noexcept {
+    return 1;
+}
+
+inline bool counting_semaphore<1>::try_acquire() {
+    auto expected = true;
+    return m_available.compare_exchange_strong(
+        expected, false, std::memory_order_acquire, std::memory_order_relaxed);
+}
+
+inline void counting_semaphore<1>::acquire() {
+    // Fast path
+    if (try_acquire()) {
+        return;
+    }
+
+    // Slow path
+    auto node = detail::intrusive_list<waiter_t>::node{};
+    m_waiters_mutex.lock();
+
+    // Re-check under lock to avoid race with release().
+    if (try_acquire()) {
+        m_waiters_mutex.unlock();
+        return;
+    }
+
+    // Guaranteed that the suspend lambda will be executed on the same thread
+    // so capturing locked mutex is fine.
+    tbb::task::suspend([this, &node](tbb::task::suspend_point sp) {
+        node.value = sp;
+        m_waiters.push_back(node);
+        m_waiters_mutex.unlock();
+    });
+
+    // Post resumption, release() has handed a permit to us.
+}
+
+inline void counting_semaphore<1>::release(std::ptrdiff_t update) {
+    assert(update >= 0);
+
+    // No permits returned
+    if (update == 0) {
+        return;
+    }
+
+    assert(m_available.load(std::memory_order_acquire) == false);
+
+    auto lock = tbb::spin_mutex::scoped_lock(m_waiters_mutex);
+
+    // Direct handoff: resume a waiter if there is any.
+    if (const auto* waiter = m_waiters.pop_front()) {
+        m_available.store(false, std::memory_order_relaxed);
+        tbb::task::resume(waiter->value);
+        return;
+    }
+
+    // No waiters: store a single permit.
+    m_available.exchange(true, std::memory_order_release);
 }
 
 }  // namespace coopsync_tbb
