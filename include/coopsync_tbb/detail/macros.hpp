bb5d2d20fd8ecb51657ce4967be68b3f74b2e5f6
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2026-02-19 09:15:55 +0100

    fix logic for custom nodiscard macro definition

--- include/coopsync_tbb/detail/macros.hpp
+++ include/coopsync_tbb/detail/macros.hpp
@@ -2,15 +2,16 @@
 
 // nodiscard attribute if supported
 
+// clang-format off
 #ifdef __has_cpp_attribute
-#if __has_cpp_attribute(nodiscard)
-#define COOPSYNC_TBB_NODISCARD [[nodiscard]]
-#endif
+  #if __has_cpp_attribute(nodiscard)
+    #define COOPSYNC_TBB_NODISCARD [[nodiscard]]
+  #else
+    #define COOPSYNC_TBB_NODISCARD
+  #endif
+#elif __cplusplus > 201703L
+  #define COOPSYNC_TBB_NODISCARD [[nodiscard]]
 #else
-#if __cplusplus > 201603L
-#define COOPSYNC_TBB_NODISCARD [[nodiscard]]
+  #define COOPSYNC_TBB_NODISCARD
 #endif
-#endif
-#ifndef COOPSYNC_TBB_NODISCARD
-#define COOPSYNC_TBB_NODISCARD
-#endif
+// clang-format on
