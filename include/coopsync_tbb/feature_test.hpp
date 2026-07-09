b53069de461b7b270c89f4cd491229cffc60572c
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2026-07-09 23:25:36 +0200

    add atomic_flag, add feature test header, define DOXYGEN macro for docs

---
+++ include/coopsync_tbb/feature_test.hpp
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2026 CERN
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/// @file
+/// @brief Feature test macros for the CoopSync_TBB public API.
+
+#pragma once
+
+#if defined(__has_include) && __has_include(<version>)
+#include <version>
+#endif
+
+#ifdef DOXYGEN
+/// @brief Macro that indicates whether atomic_flag is supported on the current platform.
+#define COOPSYNC_TBB_HAS_ATOMIC_FLAG 1
+#endif
+
+#if defined(__cpp_lib_atomic_flag_test) && __cpp_lib_atomic_flag_test >= 201907L
+#define COOPSYNC_TBB_HAS_ATOMIC_FLAG 1  // NOLINT(cppcoreguidelines-macro-usage)
+#endif
---
+++ src/atomic_flag.cpp
@@ -0,0 +1,140 @@
+// SPDX-FileCopyrightText: 2026 CERN
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "coopsync_tbb/feature_test.hpp"
+
+#if defined(COOPSYNC_TBB_HAS_ATOMIC_FLAG) && COOPSYNC_TBB_HAS_ATOMIC_FLAG == 1
+
+#include "coopsync_tbb/atomic_flag_condition.hpp"
+
+#include <atomic>
+#include <cassert>
+
+namespace coopsync_tbb {
+
+atomic_flag::~atomic_flag() {
+    assert(m_waiters.empty());  // LCOV_EXCL_LINE
+}
+
+void atomic_flag::clear(std::memory_order order) noexcept {
+    assert(order != std::memory_order_consume);  // LCOV_EXCL_LINE
+    assert(order != std::memory_order_acquire);  // LCOV_EXCL_LINE
+    assert(order != std::memory_order_acq_rel);  // LCOV_EXCL_LINE
+
+    m_value.clear(order);
+}
+
+bool atomic_flag::test_and_set(std::memory_order order) noexcept {
+    return m_value.test_and_set(order);
+}
+
+bool atomic_flag::test(std::memory_order order) const noexcept {
+    assert(order != std::memory_order_release);  // LCOV_EXCL_LINE
+    assert(order != std::memory_order_acq_rel);  // LCOV_EXCL_LINE
+
+    return m_value.test(order);
+}
+
+void atomic_flag::wait(bool old, std::memory_order order) {
+    assert(order != std::memory_order_release);  // LCOV_EXCL_LINE
+    assert(order != std::memory_order_acq_rel);  // LCOV_EXCL_LINE
+
+    while (true) {
+        if (old != m_value.test(order)) {
+            return;
+        }
+        m_waiters.wait_if(
+            [this, old, order] { return old == m_value.test(order); });
+    }
+}
+
+void atomic_flag::notify_one() {
+    m_waiters.resume_one();
+}
+
+void atomic_flag::notify_all() {
+    m_waiters.resume_all();
+}
+
+std::atomic_flag& atomic_flag::atomic() noexcept {
+    return m_value;
+}
+
+std::atomic_flag const& atomic_flag::atomic() const noexcept {
+    return m_value;
+}
+
+std::atomic_flag& atomic_flag::operator*() noexcept {
+    return m_value;
+}
+
+const std::atomic_flag& atomic_flag::operator*() const noexcept {
+    return m_value;
+}
+
+std::atomic_flag* atomic_flag::operator->() noexcept {
+    return &m_value;
+}
+
+std::atomic_flag const* atomic_flag::operator->() const noexcept {
+    return &m_value;
+}
+
+void atomic_flag_clear(atomic_flag* object) noexcept {
+    assert(object != nullptr);  // LCOV_EXCL_LINE
+    object->clear(std::memory_order_seq_cst);
+}
+
+void atomic_flag_clear_explicit(atomic_flag* object,
+                                std::memory_order order) noexcept {
+    assert(object != nullptr);  // LCOV_EXCL_LINE
+    object->clear(order);
+}
+
+bool atomic_flag_test_and_set(atomic_flag* object) noexcept {
+    assert(object != nullptr);  // LCOV_EXCL_LINE
+    return object->test_and_set(std::memory_order_seq_cst);
+}
+
+bool atomic_flag_test_and_set_explicit(atomic_flag* object,
+                                       std::memory_order order) noexcept {
+    assert(object != nullptr);  // LCOV_EXCL_LINE
+    return object->test_and_set(order);
+}
+
+bool atomic_flag_test(atomic_flag* object) noexcept {
+    assert(object != nullptr);  // LCOV_EXCL_LINE
+    return object->test(std::memory_order_seq_cst);
+}
+
+bool atomic_flag_test_explicit(atomic_flag* object,
+                               std::memory_order order) noexcept {
+    assert(object != nullptr);  // LCOV_EXCL_LINE
+    return object->test(order);
+}
+
+void atomic_wait(atomic_flag* object, bool old) {
+    assert(object != nullptr);  // LCOV_EXCL_LINE
+    object->wait(old, std::memory_order_seq_cst);
+}
+
+void atomic_wait_explicit(atomic_flag* object, bool old,
+                          std::memory_order order) {
+    assert(object != nullptr);  // LCOV_EXCL_LINE
+    object->wait(old, order);
+}
+
+void atomic_notify_one(atomic_flag* object) {
+    assert(object != nullptr);  // LCOV_EXCL_LINE
+    object->notify_one();
+}
+
+void atomic_notify_all(atomic_flag* object) {
+    assert(object != nullptr);  // LCOV_EXCL_LINE
+    object->notify_all();
+}
+
+}  // namespace coopsync_tbb
+
+#endif
