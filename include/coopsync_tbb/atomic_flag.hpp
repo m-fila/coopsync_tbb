d5ad9c566c6379aa60783999c9eb167e03fc82cd
Mateusz Jakub Fila <37295697+m-fila@users.noreply.github.com>
2026-07-22 18:19:22 +0200

    Add atomic flag (#89)
    * add empty source for version
    
    * add atomic_flag, add feature test header, define DOXYGEN macro for docs
    
    * fix format, license notice
    
    * update readme
    
    * add missing docstrings
    
    * specify C++ standard in CI
    
    * fix file name
    
    * add more tests

---
+++ include/coopsync_tbb/atomic_flag.hpp
@@ -0,0 +1,202 @@
+// SPDX-FileCopyrightText: 2026 CERN
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "coopsync_tbb/feature_test.hpp"
+
+#if defined(DOXYGEN) || (defined(COOPSYNC_TBB_HAS_ATOMIC_FLAG) && \
+                         COOPSYNC_TBB_HAS_ATOMIC_FLAG == 1)
+
+#include <atomic>
+
+#include "coopsync_tbb/detail/wait_queue.hpp"
+
+namespace coopsync_tbb {
+
+/// @brief Wrapper around std::atomic_flag that provides atomic waiting which
+/// suspends the calling task instead of blocking it as well as atomic test and
+/// set and clear operations.
+// Available only if the standard library supports \c std::atomic_flag::test and
+// \c std::atomic_flag::clear with memory order parameters (C++20). Compliance
+// can be checked with COOPSYNC_TBB_HAS_ATOMIC_FLAG.
+class atomic_flag {
+
+    public:
+    /// @brief Constructs a new atomic_flag. Initializes to clear state.
+    atomic_flag() = default;
+
+    /// @brief The atomic_flag is not copy-constructible.
+    atomic_flag(const atomic_flag&) = delete;
+
+    /// @brief The atomic_flag is not copy-assignable.
+    atomic_flag& operator=(const atomic_flag&) = delete;
+
+    /// @brief The atomic_flag is not move-constructible.
+    atomic_flag(atomic_flag&&) = delete;
+
+    /// @brief The atomic_flag is not move-assignable.
+    atomic_flag& operator=(atomic_flag&&) = delete;
+
+    /// @brief Destroys the atomic_flag.
+    /// @note The destructor must not be called while there are still tasks
+    /// waiting on the atomic_flag. The destructor does not notify or
+    /// resume any waiting tasks.
+    ~atomic_flag();
+
+    /// @brief Atomically sets the state to clear.
+    /// @param order The memory order to use for clearing the flag. Must not be
+    /// \c std::memory_order_consume, \c std::memory_order_acquire or \c
+    /// std::memory_order_acq_rel.
+    void clear(std::memory_order order = std::memory_order_seq_cst) noexcept;
+
+    /// @brief Atomically sets the state to set and returns the previous state.
+    /// @param order The memory order to use for setting the flag.
+    bool test_and_set(
+        std::memory_order order = std::memory_order_seq_cst) noexcept;
+
+    /// @brief Atomically reads the current state and returns it.
+    /// @param order The memory order to use for reading the flag. Must not be
+    /// \c std::memory_order_release, \c std::memory_order_acq_rel.
+    bool test(
+        std::memory_order order = std::memory_order_seq_cst) const noexcept;
+
+    /// @brief Suspends the calling task until the state is not
+    /// equal to specified value. If the state is not equal to
+    /// old, the function returns immediately. Otherwise, the task is suspended.
+    /// The task will be resumed once notify_one or notify_all is called and the
+    /// state is not equal to old.
+    /// @param old The value to compare the internal atomic value to. The task
+    /// will be suspended while the internal atomic value is equal to old.
+    /// @param order The memory order to use for loading the internal atomic
+    /// value. Must not be \c std::memory_order_release or \c
+    /// std::memory_order_acq_rel.
+    /// @note Due to ABA problem, transitions from old to a different value and
+    /// back to old may be missed.
+    void wait(bool old, std::memory_order order = std::memory_order_seq_cst);
+
+    /// @brief Resumes one task suspended waiting on this atomic_flag, if
+    /// there is any.
+    void notify_one();
+
+    /// @brief Resumes all tasks suspended waiting on this atomic_flag.
+    void notify_all();
+
+    /// @brief Access to the internal atomic value.
+    /// @return Reference to the internal atomic value. Remains valid for the
+    /// lifetime of the atomic_flag.
+    std::atomic_flag& atomic() noexcept;
+
+    /// @brief Access to the internal atomic value.
+    /// @return Const reference to the internal atomic value. Remains valid for
+    /// the lifetime of the atomic_flag.
+    const std::atomic_flag& atomic() const noexcept;
+
+    /// @brief Access to the internal atomic value.
+    /// @return Reference to the internal atomic value. Remains valid for the
+    /// lifetime of the atomic_flag.
+    std::atomic_flag& operator*() noexcept;
+
+    /// @brief Access to the internal atomic value.
+    /// @return Const reference to the internal atomic value. Remains valid for
+    /// the lifetime of the atomic_flag.
+    const std::atomic_flag& operator*() const noexcept;
+
+    /// @brief Access to the internal atomic value.
+    /// @return Pointer to the internal atomic value. Remains valid for the
+    /// lifetime of the atomic_flag.
+    std::atomic_flag* operator->() noexcept;
+
+    /// @brief Access to the internal atomic value.
+    /// @return Const pointer to the internal atomic value. Remains valid for
+    /// the lifetime of the atomic_flag.
+    const std::atomic_flag* operator->() const noexcept;
+
+    private:
+    std::atomic_flag m_value{};
+    detail::wait_queue m_waiters;
+};
+
+/// @brief Atomically sets the state to clear.
+/// Same as \c object->clear().
+/// @param object Pointer to the atomic_flag to clear.
+void atomic_flag_clear(atomic_flag* object) noexcept;
+
+/// @brief Atomically sets the state to clear.
+/// Same as \c object->clear(order).
+/// @param object Pointer to the atomic_flag to clear.
+/// @param order The memory order to use for clearing the flag. Must not be
+/// \c std::memory_order_consume, \c std::memory_order_acquire or \c
+/// std::memory_order_acq_rel.
+void atomic_flag_clear_explicit(atomic_flag* object,
+                                std::memory_order order) noexcept;
+
+/// @brief Atomically sets the state to set and returns the previous state.
+/// Same as \c object->test_and_set().
+/// @param object Pointer to the atomic_flag to set.
+bool atomic_flag_test_and_set(atomic_flag* object) noexcept;
+
+/// @brief Atomically sets the state to set and returns the previous state.
+/// Same as \c object->test_and_set(order).
+/// @param object Pointer to the atomic_flag to set.
+/// @param order The memory order to use for setting the flag.
+bool atomic_flag_test_and_set_explicit(atomic_flag* object,
+                                       std::memory_order order) noexcept;
+
+/// @brief Atomically reads the current state and returns it.
+/// Same as \c object->test().
+/// @param object Pointer to the atomic_flag to read.
+bool atomic_flag_test(atomic_flag* object) noexcept;
+
+/// @brief Atomically reads the current state and returns it.
+/// Same as \c object->test(order).
+/// @param object Pointer to the atomic_flag to read.
+/// @param order The memory order to use for reading the flag. Must not be
+/// \c std::memory_order_release, \c std::memory_order_acq_rel.
+bool atomic_flag_test_explicit(atomic_flag* object,
+                               std::memory_order order) noexcept;
+
+/// @brief Suspends the calling task until the state is not equal to specified
+/// value. If the internal atomic the state is not equal to old, the function
+/// returns immediately. Otherwise, the task is suspended. The task will be
+/// resumed once associated notify function is called and the state is not equal
+/// to old. Same as \c object->wait(old).
+/// @param object Pointer to the atomic_flag to wait on.
+/// @param old The value to compare the state to. The task will
+/// be suspended while the state is equal to old.
+/// @note Due to ABA problem, transitions from old to a different value and
+/// back to old may be missed.
+void atomic_wait(atomic_flag* object, bool old);
+
+/// @brief Suspends the calling task until the state is not equal to specified
+/// value. If the internal atomic the state is not equal to old, the function
+/// returns immediately. Otherwise, the task is suspended. The task will be
+/// resumed once associated notify function is called and the state is not equal
+/// to old. Same as \c object->wait(old, order).
+/// @param object Pointer to the atomic_flag to wait on.
+/// @param old The value to compare the state to. The task will
+/// be suspended while the state is equal to old.
+/// @param order The memory order to use for loading the internal atomic value.
+/// Must not be \c std::memory_order_release or \c std::memory_order_acq_rel.
+/// @note Due to ABA problem, transitions from old to a different value and
+/// back to old may be missed.
+void atomic_wait_explicit(atomic_flag* object, bool old,
+                          std::memory_order order);
+
+/// @brief Resumes one task suspended waiting on the atomic_flag, if there
+/// is any.
+/// Same as \c object.notify_one().
+/// @param object Pointer to the atomic_flag to notify on.
+void atomic_notify_one(atomic_flag* object);
+
+/// @brief Resumes all tasks suspended waiting on the atomic_flag.
+/// Same as \c object.notify_all().
+/// @param object Pointer to the atomic_flag to notify on.
+void atomic_notify_all(atomic_flag* object);
+
+}  // namespace coopsync_tbb
+
+#else
+#error "Atomic flag condition variable is not supported on this platform."
+#endif
---
+++ tests/atomic_flag_test.cpp
@@ -0,0 +1,191 @@
+// SPDX-FileCopyrightText: 2026 CERN
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "coopsync_tbb/feature_test.hpp"
+
+#if defined(COOPSYNC_TBB_HAS_ATOMIC_FLAG) && COOPSYNC_TBB_HAS_ATOMIC_FLAG == 1
+
+#include <gtest/gtest.h>
+#include <oneapi/tbb/parallel_for.h>
+
+#include <atomic>
+
+#include "coopsync_tbb/atomic_flag.hpp"
+
+TEST(AtomicFlag, BasicOperations) {
+    auto flag = coopsync_tbb::atomic_flag();
+
+    ASSERT_FALSE(flag.test());
+    ASSERT_FALSE(flag.test_and_set());
+    ASSERT_TRUE(flag.test());
+    flag.clear();
+    ASSERT_FALSE(flag.test());
+}
+
+TEST(AtomicFlag, BasicOperationsFreeFunctions) {
+    auto flag = coopsync_tbb::atomic_flag();
+
+    ASSERT_FALSE(coopsync_tbb::atomic_flag_test(&flag));
+    ASSERT_FALSE(coopsync_tbb::atomic_flag_test_and_set(&flag));
+    ASSERT_TRUE(coopsync_tbb::atomic_flag_test(&flag));
+    coopsync_tbb::atomic_flag_clear(&flag);
+    ASSERT_FALSE(coopsync_tbb::atomic_flag_test(&flag));
+}
+
+TEST(AtomicFlag, Accessors) {
+    auto flag = coopsync_tbb::atomic_flag();
+
+    auto& atomic_ref = flag.atomic();
+    auto& deref_ref = *flag;
+    auto* atomic_ptr = flag.operator->();
+
+    ASSERT_EQ(&atomic_ref, &deref_ref);
+    ASSERT_EQ(&atomic_ref, atomic_ptr);
+
+    atomic_ref.test_and_set(std::memory_order_relaxed);
+    ASSERT_TRUE(deref_ref.test(std::memory_order_relaxed));
+
+    (*flag).clear(std::memory_order_relaxed);
+    ASSERT_FALSE(flag->test(std::memory_order_relaxed));
+
+    flag->test_and_set(std::memory_order_relaxed);
+    ASSERT_TRUE(flag.atomic().test(std::memory_order_relaxed));
+
+    const auto& cflag = flag;
+
+    const auto& const_atomic_ref = cflag.atomic();
+    const auto& const_deref_ref = *cflag;
+    const auto* const_atomic_ptr = cflag.operator->();
+
+    ASSERT_EQ(&const_atomic_ref, &const_deref_ref);
+    ASSERT_EQ(&const_atomic_ref, const_atomic_ptr);
+    ASSERT_TRUE(const_atomic_ptr->test(std::memory_order_relaxed));
+}
+
+TEST(AtomicFlag, WaitReturnsImmediatelyWhenNotEqual) {
+    auto flag = coopsync_tbb::atomic_flag();
+    // Should not suspend or block.
+    flag.wait(true);
+}
+
+TEST(AtomicFlag, NotifyWithNoWaiters) {
+    auto flag = coopsync_tbb::atomic_flag();
+    // Should not crash or do anything.
+    flag.notify_one();
+    flag.notify_all();
+}
+
+TEST(AtomicFlag, NotifyOneUnblocksOne) {
+    auto flag = coopsync_tbb::atomic_flag();
+
+    auto done = std::atomic<int>{0};
+
+    tbb::parallel_for(0, 2, [&](int i) {
+        if (i == 0) {
+            flag.wait(false);
+            done.store(1, std::memory_order_relaxed);
+        } else {
+            flag.test_and_set(std::memory_order_relaxed);
+            flag.notify_one();
+        }
+    });
+
+    ASSERT_EQ(done.load(std::memory_order_relaxed), 1);
+}
+
+TEST(AtomicFlag, NotifyAllUnblocksAll) {
+    auto flag = coopsync_tbb::atomic_flag();
+
+    auto done = std::atomic<int>{0};
+
+    tbb::parallel_for(0, 3, [&](int i) {
+        if (i == 0) {
+            flag.wait(false);
+            done.fetch_add(1, std::memory_order_relaxed);
+        } else if (i == 1) {
+            flag.wait(false);
+            done.fetch_add(1, std::memory_order_relaxed);
+        } else {
+            flag.test_and_set(std::memory_order_relaxed);
+            flag.notify_all();
+        }
+    });
+
+    ASSERT_EQ(done.load(std::memory_order_relaxed), 2);
+}
+
+TEST(AtomicFlag, WaitFreeFunctions) {
+
+    auto flag = coopsync_tbb::atomic_flag();
+    auto done = std::atomic<int>{0};
+
+    tbb::parallel_for(0, 2, [&](int i) {
+        if (i == 0) {
+            coopsync_tbb::atomic_wait(&flag, false);
+            done.store(1, std::memory_order_relaxed);
+        } else {
+            coopsync_tbb::atomic_flag_test_and_set(&flag);
+            coopsync_tbb::atomic_notify_one(&flag);
+        }
+    });
+
+    ASSERT_EQ(done.load(std::memory_order_relaxed), 1);
+}
+
+TEST(AtomicFlag, NotifyAllFreeFunctions) {
+
+    auto flag = coopsync_tbb::atomic_flag();
+    auto done = std::atomic<int>{0};
+
+    tbb::parallel_for(0, 3, [&](int i) {
+        if (i == 0) {
+            coopsync_tbb::atomic_wait(&flag, false);
+            done.fetch_add(1, std::memory_order_relaxed);
+        } else if (i == 1) {
+            coopsync_tbb::atomic_wait(&flag, false);
+            done.fetch_add(1, std::memory_order_relaxed);
+        } else {
+            coopsync_tbb::atomic_flag_test_and_set(&flag);
+            coopsync_tbb::atomic_notify_all(&flag);
+        }
+    });
+
+    ASSERT_EQ(done.load(std::memory_order_relaxed), 2);
+}
+
+TEST(AtomicFlag, FreeFunctionsWaitExplicit) {
+
+    auto flag = coopsync_tbb::atomic_flag();
+    auto done = std::atomic<int>{0};
+
+    tbb::parallel_for(0, 2, [&](int i) {
+        if (i == 0) {
+            coopsync_tbb::atomic_wait_explicit(&flag, false,
+                                               std::memory_order_relaxed);
+            done.store(1, std::memory_order_relaxed);
+        } else {
+            coopsync_tbb::atomic_flag_test_and_set_explicit(
+                &flag, std::memory_order_relaxed);
+            coopsync_tbb::atomic_notify_one(&flag);
+        }
+    });
+
+    ASSERT_EQ(done.load(std::memory_order_relaxed), 1);
+}
+
+TEST(AtomicFlag, FreeFunctionsTestExplicit) {
+    auto flag = coopsync_tbb::atomic_flag();
+
+    ASSERT_FALSE(coopsync_tbb::atomic_flag_test_explicit(
+        &flag, std::memory_order_relaxed));
+    coopsync_tbb::atomic_flag_test_and_set_explicit(&flag,
+                                                    std::memory_order_relaxed);
+    ASSERT_TRUE(coopsync_tbb::atomic_flag_test_explicit(
+        &flag, std::memory_order_relaxed));
+    coopsync_tbb::atomic_flag_clear_explicit(&flag, std::memory_order_relaxed);
+    ASSERT_FALSE(coopsync_tbb::atomic_flag_test_explicit(
+        &flag, std::memory_order_relaxed));
+}
+
+#endif
